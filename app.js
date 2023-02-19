721246274a25cd7dad3bc85f19209b23f99f9385
Rantaa <gusranlo@student.gu.se>
2023-02-19 15:51:08 +0100

    Fix for bug in DB url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb:http://127.0.0.1:27017/wikiDB');
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
 
 const articleSchema = {
     title: String,
